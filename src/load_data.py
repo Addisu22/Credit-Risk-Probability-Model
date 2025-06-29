cd46fa6a9a5e6720943b0632341c790c8b6ef27e
Addisu22 <liveaddisu@gmail.com>
2025-06-29 22:48:35 +0300

    Exploratory Data Analysis

--- src/load_data.py
+++ src/load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def load_data(file_path):
+def load_dataeda(file_path):
     try:
         df = pd.read_csv(file_path)
         print(" Data loaded successfully.")
