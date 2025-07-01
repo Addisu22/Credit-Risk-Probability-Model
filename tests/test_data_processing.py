024ba53d102e1fb2ef49a9f7ae66448038408e11
Addisu22 <liveaddisu@gmail.com>
2025-07-01 22:26:10 +0300

    Model Training and Tracking

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -5,11 +5,11 @@ def test_load_data():
     df = load_data("Data/processed/cleaned_data.csv")
     assert df is not None
     assert isinstance(df, pd.DataFrame)
-    print("✅ test_load_data passed")
+    print("test_load_data passed")
 
 def test_split_data():
     df = load_data("Data/processed/cleaned_data.csv")
     X_train, X_test, y_train, y_test = split_data(df, "fraudresult")
     assert X_train.shape[0] > 0
     assert y_test.shape[0] > 0
-    print("✅ test_split_data passed")
+    print("test_split_data passed")
